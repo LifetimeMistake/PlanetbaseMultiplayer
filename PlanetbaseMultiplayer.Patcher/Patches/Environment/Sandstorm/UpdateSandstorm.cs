d30f77e46648804b6ef89a72cbd6b9aece60eb4f
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2022-05-22 06:40:29 -0700

    fix because geatlolz

--- PlanetbaseMultiplayer.Patcher/Patches/Environment/Sandstorm/UpdateSandstorm.cs
+++ PlanetbaseMultiplayer.Patcher/Patches/Environment/Sandstorm/UpdateSandstorm.cs
@@ -15,7 +15,7 @@ namespace PlanetbaseMultiplayer.Patcher.Patches.Environment.Sandstorm
     [HarmonyPatch(typeof(Planetbase.Sandstorm), "update")]
     public class UpdateSandstorm
     {
-        static bool Prefix(Planetbase.Sandstorm __instance)
+        static bool Prefix(Planetbase.Sandstorm __instance, ref float timeStep)
         {
             if (Multiplayer.Client == null)
                 return true;
@@ -24,16 +24,44 @@ namespace PlanetbaseMultiplayer.Patcher.Patches.Environment.Sandstorm
             if (simulationOwner == null || simulationOwner.Value != Multiplayer.Client.LocalPlayer)
                 return false; // Player isn't the simulation owner
 
-            PlanetbaseMultiplayer.Client.Environment.DisasterManager disasterManager = Multiplayer.Client.DisasterManager;
-            Type instance = __instance.GetType();
+            Type sandstormType = __instance.GetType();
+            FieldInfo mSandstormInProgress = Reflection.GetPrivateFieldOrThrow(sandstormType, "mSandstormInProgress", true);
+            bool sandstormInProgress = (bool)Reflection.GetInstanceFieldValue(__instance, mSandstormInProgress);
+            if(sandstormInProgress)
+            {
+                FieldInfo mTimeInfo = Reflection.GetPrivateFieldOrThrow(sandstormType, "mTime", true);
+                FieldInfo mSandstormTimeInfo = Reflection.GetPrivateFieldOrThrow(sandstormType, "mStandstormTime", true);
+                MethodInfo onEndInfo = Reflection.GetPrivateMethodOrThrow(sandstormType, "onEnd", true);
 
-            FieldInfo mTimeInfo = Reflection.GetPrivateFieldOrThrow(instance, "mTime", true);
+                float mTime = (float)Reflection.GetInstanceFieldValue(__instance, mTimeInfo);
+                float mSandstormTime = (float)Reflection.GetInstanceFieldValue(__instance, mSandstormTimeInfo);
 
-            float currentTime = (float)Reflection.GetInstanceFieldValue(__instance, mTimeInfo);
+                if (mTime > mSandstormTime)
+                {
+                    // End sandstorm
+                    Reflection.InvokeInstanceMethod(__instance, onEndInfo, new object[] { });
+                }
+            }
+            else
+            {
+                // Create a new sandstorm
+                MethodInfo updateDetectionInfo = Reflection.GetPrivateMethodOrThrow(sandstormType, "updateDetection", true);
+                MethodInfo decideNextSandstormInfo = Reflection.GetPrivateMethodOrThrow(sandstormType, "decideNextSandstorm", true);
+                FieldInfo mTimeToNextSandstorminfo = Reflection.GetPrivateFieldOrThrow(sandstormType, "mTimeToNextSandstorm", true);
+                float mTimeToNextSandstorm = (float)Reflection.GetInstanceFieldValue(__instance, mTimeToNextSandstorminfo);
 
-            disasterManager.UpdateDisaster(currentTime);
+                Reflection.InvokeInstanceMethod(sandstormType, updateDetectionInfo, new[] { mTimeToNextSandstorm, timeStep });
+                mTimeToNextSandstorm -= timeStep;
+                Reflection.SetInstanceFieldValue(__instance, mTimeToNextSandstorminfo, mTimeToNextSandstorm);
+                if (mTimeToNextSandstorm < 0f)
+                {
+                    // Trigger sandstorm
+                    __instance.trigger();
+                    Reflection.InvokeInstanceMethod(__instance, decideNextSandstormInfo, new object[] { });
+                }
+            }
 
-            return false;
+            return true;
         }
     }
 }
