e6abf7722f6e35f66d116fc1a46933dc56e3e668
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2022-07-09 14:24:39 -0700

    Implement service locator

--- PlanetbaseMultiplayer.Model/Utils/Reflection.cs
+++ PlanetbaseMultiplayer.Model/Utils/Reflection.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace PlanetbaseMultiplayer.Model
+namespace PlanetbaseMultiplayer.Model.Utils
 {
     public static class Reflection
     {
