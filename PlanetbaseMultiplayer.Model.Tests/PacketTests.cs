2a2a89db968106d81cf0c67749373605c6ebb858
greatlolz <greatlolz.676@gmail.com>
2022-07-11 16:05:45 +0200

    packet serializable NUnit test

---
+++ PlanetbaseMultiplayer.Model.Tests/PacketTests.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using PlanetbaseMultiplayer.Model;
+using PlanetbaseMultiplayer.Model.Packets;
+using NUnit.Framework;
+
+namespace PlanetbaseMultiplayer.Model.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void PacketSerializableTest()
+        {
+            Assembly? ass = Assembly.GetAssembly(typeof(Packet));
+            List<Type> invalidTypes = new List<Type>();
+
+            if (ass == null)
+            {
+                throw new Exception("Could not find assembly");
+            }
+
+            foreach (Type packetType in ass.GetTypes().Where(p => p.IsAssignableTo(typeof(Packet))))
+            {
+                if (packetType.GetCustomAttribute(typeof(SerializableAttribute)) == null)
+                {
+                    invalidTypes.Add(packetType);
+                    throw new Exception($"Packet type {packetType} in {packetType.Namespace} has no Serializable attribute");
+                }
+            }
+
+            Assert.That(invalidTypes.Count, Is.EqualTo(0));
+        }
+    }
+}
